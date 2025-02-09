0e2ac49acceccdd18df687380e4e1e9323d3a2e8
SameerGangurde71 <sameergangurde71@gmail.com>
2025-02-09 18:20:29 +0530

    initialize React project with Vite setup and basic components

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+   <h1>Chai Aur React with Vite {username}</h1>
+   <p>test</p>
+   </>
+  )
+}
+
+export default App
+
+
